bab099627913e944a93bdecffc1ac64e02f91b12
Pritthijit Nath <nathzi1505@Pritthijits-MacBook-Pro.local>
2020-05-14 23:01:21 +0530

    Added smile detection

--- Face_Recognition/smile_recognition.py
+++ Face_Recognition/smile_recognition.py
@@ -2,10 +2,10 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('./haarcascade_eye.xml')
-
+smile_cascade = cv2.CascadeClassifier('./haarcascade_smile.xml')
 
 def detect(gray, frame):
-    """ Perform detections for face and eyes """
+    """ Perform emotion detection """
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -15,6 +15,10 @@ def detect(gray, frame):
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey),
                           (ex + ew, ey + eh), (0, 255, 0), 2)
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy),
+                          (sx + sw, sy + sh), (0, 0, 255), 2)
     return frame
  
 
